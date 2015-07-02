165f3d4c490f970cf7ab192130c1c71d9f0c2998
Christopher Buja <csbuja@ChristophersAir.attlocal.net>
2015-07-02 04:33:51 -0700

    not fully ready. still need to debug and sort out what to do about the gaussian created for a feature in a class that has zero variance

--- fit.js
+++ fit.js
@@ -1,38 +1,74 @@
 var _ = require("underscore");
+var stats = require("stats-lite");
+var gaussian = require('gaussian');
 
-//requires that y.length is equal to binDattaArray.length 
-function Fitter(X,y,numClasses) {
-	if(!numClasses){
-		numClasses = 0;
-		classesSeen = {};
-		for( var i = 0; i<X.length; ++i){
-			if (!classesSeen[y[i]]) {
-				classesSeen[y[i]] = true;
-				++numClasses;
-			}
+//requires that y.length is equal to X.length 
+//requires that there is at least 1 row
+function Fitter(X,y) {
+	if (X.length === 0 || X.length !== y.length) {
+		throw new Error("y.length is equal to X.length and there should be at least one row");
+		return {};
+	}
+
+	var numClasses = 0;
+	var classesSeen = {};
+	for( var i = 0; i<X.length; ++i){
+		if (!classesSeen[y[i]]) {
+			classesSeen[y[i]] = true;
+			++numClasses;
 		}
 	}
 
 	//initialize counts to 0 for each class and features for each class
-	classCount = _.extend(classesSeen);
-	featuresByClass = _.extend(classesSeen);
+	var classCount = _.extend(classesSeen);
+	var featuresByClass = _.extend(classesSeen);
 	_.each(classCount, function(v,i){
 		classCount[i] = 0;
-		featuresByClass[i] = Array.apply(null, Array(X[0].length)).map(Number.prototype.valueOf,0);
+		featuresByClass[i] = Array.apply(null, Array(X[0].length)).map(Array.prototype.valueOf,[]);
 	});
-	
+
 	var fitter = {
+		"classesSeen" : classesSeen,
 		"numClasses": numClasses,
 		"classCount" : classCount,
+		"piClass" : _.extend(classCount),
 		"featuresByClass" : featuresByClass,
 		"X" : X,
 		"y" : y,
-		"fit": function() {
-			_.each(binDattaArray, function(v,i) {
-				var classLabel = y[i];
+		"calcStatisticsForFeatureOfClass" : function(){
+			var self = this;
+			_.each(self.featuresByClass, function(v,k){
+				_.each(v,function(val,i){
+					var mean = stats.mean(val);
 
-			}
+					// THIS MAY NEED TO BE FIXED BECAUSE CANT COMPUTE GAUSSIAN WITH 0 VARIANCE
+					var variance =  stats.variance(val);
+					self.featuresByClass[k][i] = gaussian(mean,variance);
+				})
+			});
+		},
+		"calcPiClass" : function() {
+			var self = this;
+			_.each(self.piClass,function(v,k){
+				self.piClass[k] = self.classCount[k]/self.X.length
+			});
+		},
+		"fit": function() {
+			var self = this;
+
+			_.each(self.X, function(v,i) {
+				var classLabel = y[i];
+				self.classCount[y[i]] +=1;
+				_.each(self.X[i], function(va,j){
+					self.featuresByClass[classLabel][j].push(self.X[i][j]); 
+				});
+			});
+			self.calcPiClass();
+			self.calcStatisticsForFeatureOfClass();
 		}
 	}
 	return fitter;
 }
+
+module.exports = Fitter;
+
---
+++ predictor.js
@@ -0,0 +1,56 @@
+var _ = require("underscore");
+
+function Predictor(fitter, X) {
+	if (!X.length || fitter.X.length || X.length !== fitter.X.length || X.length[0]!==fitter.X.length) {
+		throw new Error("Invalid X or fitter due to size.");
+	}
+
+	var predictor = {
+		"fitter" : fitter,
+		"X" : X,
+		"calcMax" : function(obj){ //debugged
+			var i = 0;
+			var maxClass=0;
+			var maxNumber=0;
+
+			_.each(obj,function(v,k){
+				if (!i) {
+					maxNumber=v;
+					maxClass=k;
+				}
+				if (v>maxNumber) {
+					maxClass = k;
+					maxNumber = v;
+				}
+				++i;
+			})
+			return maxClass;
+		},
+		"predict" : function() {
+			var self = this;
+			y = [];
+			//initialize L and run the predictor
+			_.each(self.X,function(v,i){
+				var currentExample = {};
+				_.each(self.fitter.piClass,function(val,k){
+					var prior = Math.log(val);
+					var j = 0;
+					var likelihood = Math.log(_.reduce(self.fitter.featuresByClass[k],function(memo,v){
+						var xFeatureValue = self.X[i][j];
+						++j;
+						return memo * v.pdf(xFeatureValue);
+					},1));
+					currentExample[k] = prior + likelihood; //from log rules
+				});
+
+				y.push(self.calcMax(currentExample));
+			});
+			return y;
+		}
+	};
+
+	return predictor;
+
+}
+
+module.exports = Predictor;
